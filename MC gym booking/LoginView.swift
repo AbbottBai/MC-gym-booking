453538cba02ff316b5f5d740c9057eca0697e92a
AbbottBai <abbottrun08@hotmail.com>
2024-11-18 22:55:49 +0000

    Started project

---
+++ MC gym booking/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  MC gym booking
+//
+//  Created by user267421 on 11/18/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
