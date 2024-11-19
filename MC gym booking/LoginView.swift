32579ea912e0352db1ef4ef912a4303e05268eb5
AbbottBai <abbottrun08@hotmail.com>
2024-11-19 09:07:06 +0000

    start of project

--- MC gym booking/LoginView.swift
+++ MC gym booking/LoginView.swift
@@ -6,13 +6,95 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
-struct SwiftUIView: View {
+struct LoginView: View {
+    @StateObject private var auth = Authentication()
+    
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    class Authentication: ObservableObject {
+        @Published var signed_in: Bool = false
+        
+        func email_signup(email: String, password: String, completion: @escaping (Bool, String?) -> Void) {
+            Auth.auth().createUser(withEmail: email, password: password) {(result,error) in
+                if let error = error {
+                    completion(false, "Error: \(error.localizedDescription)")
+                } else {
+                    self.signed_in = true
+                    completion(true,nil)
+                }
+            }
+        }
+        
+        func email_login(email: String, password: String, completion: @escaping (Bool, String?) -> Void) {
+            Auth.auth().signIn(withEmail: email, password: password) {(result, error) in
+                if let error = error {
+                    completion(false, "error:\(error.localizedDescription)")
+                } else {
+                    self.signed_in = true
+                    completion (true, nil)
+                }
+            }
+        }
+        
+        func email_logout(completion: @escaping(Bool, String?) -> Void) {
+            do{
+                try Auth.auth().signOut()
+                completion(true, nil)
+                self.signed_in = false
+            } catch let error {
+                completion(false, "Error: \(error.localizedDescription)")
+                self.signed_in = true
+            }
+        }
+        
+        func check_email_auth(completion: @escaping(Bool, String?) -> Void) {
+            if let user = Auth.auth().currentUser {
+                completion(true,user.email)
+            } else{
+                completion(false,nil)
+            }
+        }
+        
+        func get_current_email() -> User? {
+            return Auth.auth().currentUser
+        }
+        
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            TextField("Enter email", text: $email)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .autocapitalization(.none)
+            
+            SecureField("Enter password", text: $password)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button("Sign up") {
+                auth.email_signup(email: self.email, password: self.password) { success, error in
+                    if success {
+                        print("Signup successful")
+                    } else {
+                        print("Signup failed due to \(error ?? "Unknown error")")
+                    }
+                }
+            }
+        }
+            
     }
 }
 
+
+
 #Preview {
-    SwiftUIView()
+    LoginView()
 }
